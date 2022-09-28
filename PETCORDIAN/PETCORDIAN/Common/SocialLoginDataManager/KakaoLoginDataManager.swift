f5a9cd97d1d45d4abb1c2ee2089f7f33e4f763dd
Hyunwoo <heoun3089@gmail.com>
2022-09-28 17:15:13 +0900

    feat: kakao login 웹 브라우저에서 실행하는 로직, 계정 연결 상태 해제하는 메소드 추가

--- PETCORDIAN/PETCORDIAN/Common/SocialLoginDataManager/KakaoLoginDataManager.swift
+++ PETCORDIAN/PETCORDIAN/Common/SocialLoginDataManager/KakaoLoginDataManager.swift
@@ -16,7 +16,7 @@ class KakaoLoginDataManager {
   
   func login(completion: @escaping (_ token: String?) -> ()) {
     if (UserApi.isKakaoTalkLoginAvailable()) {
-      UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+      UserApi.shared.loginWithKakaoTalk {( oauthToken, error) in
         if let error = error {
           print(error)
         }
@@ -30,15 +30,24 @@ class KakaoLoginDataManager {
         }
       }
     } else {
-      print("카카오톡이 설치되어 있지 않을 경우, 처리할 로직")
+      // 카카오톡이 설치되어 있지 않을 경우, 처리할 로직
+      UserApi.shared.loginWithKakaoAccount { (oauthToken, error) in
+        if let error = error {
+          print(error)
+        }
+        
+        DispatchQueue.main.async {
+          completion(oauthToken?.accessToken)
+        }
+      }
     }
   }
   
-  func logout(completion: @escaping () -> ()) {
+  func logout(vc: BaseViewController, completion: @escaping () -> ()) {
     // 로그아웃은 요청 성공 여부와 관계없이 토큰을 삭제 처리
     UserApi.shared.logout {(error) in
       if let error = error {
-        print(error)
+        vc.showAlert(title: "에러", message: "로그아웃에 실패헀습니다\n\(error.localizedDescription).", actionHandler: nil)
       } else {
         DispatchQueue.main.async {
           completion()
@@ -59,6 +68,16 @@ class KakaoLoginDataManager {
         DispatchQueue.main.async {
           completion(user?.id)
         }
+      }
+    }
+  }
+  
+  func deleteUser(vc: BaseViewController) {
+    UserApi.shared.unlink { error in
+      if let error = error {
+        vc.showAlert(title: "에러", message: "카카오 연결 끊기에 실패헀습니다\n\(error.localizedDescription).", actionHandler: nil)
+      } else {
+        vc.showAlert(title: "알림", message: "카카오계정의 연결 상태를 해제했습니다.", actionHandler: nil)
       }
     }
   }
